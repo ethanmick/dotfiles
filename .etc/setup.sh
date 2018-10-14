d8ee2d6cf3a94e51be3178749170e482e7c94529
Ethan Mick <ethan@ethanmick.com>
2018-10-14 19:23:01 -0400

    Add bin and etc

---
+++ .etc/setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Figure out if this works...
+xcode-select --install
+
+brew cask install gpgtools
+brew install fzf
+brew install go
+brew install google-java-format
+brew install jq
+brew install maven
+brew install tmux
